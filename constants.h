f23493dd2a09c80fa035dd9259862ea1abb655cb
Juan Gabito <gabitoju@gmail.com>
2025-11-04 10:01:31 -0300

    Increase buffer size

--- constants.h
+++ constants.h
@@ -4,7 +4,7 @@
 #define CONFIG_FILE "/etc/gabitojusrv/server.conf"
 #define DEFAULT_PORT 8080
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 4096
 #define HTTP_VERSION "HTTP/1.1"
 #define HTML_CONTENT_TYPE "text/html"
 #define WELCOME_MESSAGE "<b>Welcome to this little HTTP sever</b>"
